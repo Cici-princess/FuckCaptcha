6f59cdd4b265fcf41bcbe1a330a369992c9a963d
Markshawn <33591398+ShawnMark@users.noreply.github.com>
2020-02-29 08:45:24 +0800

    实现了mnist手写集的识别

--- models/pytorch_test/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# -----------------------------------
-# @CreateTime   : 2020/2/29 2:15
-# @Author       : Mark Shawn
-# @Email        : shawninjuly@gmai.com
-# ------------------------------------
